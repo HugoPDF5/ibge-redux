8af555ac077173efed31b5b2337541e93f93e9d4
Hugo <hugopatricio51@gmail.com>
2022-12-13 22:41:32 -0300

    feat: showing query results on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import { callAPI } from '../services/reqs'
 import { changeCounty, changeInfo } from "./store/states/states.actions"
-
+import './app.scss'
 
 function App() {
   const dispatch = useDispatch()
@@ -32,23 +31,26 @@ function App() {
 
   return (
     <>
-      <select className="form-select-md" aria-label="Default select example" name="state" id="state" onChange={handleChangeState}>
-        <option>Selecione um estado</option>
-        {states.map(state => {
-          return <option key={state.id} value={state.sigla}> {state.nome}</option>
-        })}
-      </select>
+      <h2>Consulta de informações por município</h2>
+      <div className="selects">
+        <select className="form-select-md" aria-label="Default select example" name="state" id="state" onChange={handleChangeState}>
+          <option>Selecione um estado</option>
+          {states.map(state => {
+            return <option key={state.id} value={state.sigla}> {state.nome}</option>
+          })}
+        </select>
+        <select className="form-select-md" aria-label="Default select example" name="county" id="county" onChange={handleChangeCounty}>
+          <option>Selecione um Município</option>
+          {counties.map(county => {
+            return <option key={county.id} value={county.id}> {county.nome}</option>
+          })}
+        </select>
+      </div>
 
-      <select className="form-select-md" aria-label="Default select example" name="county" id="county" onChange={handleChangeCounty}>
-        {counties.map(county => {
-          return <option key={county.id} value={county.id}> {county.nome}</option>
-        })}
-      </select>
-
-      <table className="table">
+      <table className="table table-striped">
         <thead>
           <tr>
-            <th> Nome </th>
+            <th> Município </th>
             <th> Microrregião </th>
             <th> Mesorregião </th>
             <th> Região </th>
@@ -57,20 +59,17 @@ function App() {
         </thead>
 
         <tbody className="table-group-divider">
-          {table.map(item => {
-            return <tr>
-              <td> {item.name}</td>
-              <td> {item.microregion}</td>
-              <td> {item.mesoregion}</td>
-              <td> {item.region}</td>
-              <td> {item.uf}</td>
+          {table.map((item, index) => {
+            return <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.microregion}</td>
+              <td>{item.mesoregion}</td>
+              <td>{item.region}</td>
+              <td>{item.uf}</td>
             </tr>
           })}
         </tbody>
       </table>
-
-
-
 
     </>
   )
